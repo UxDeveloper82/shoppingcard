2f00544e6f18241fe46fda62571f60f0a23eadc8
UxDeveloper82 <electronicsmf@aol.com>
2020-03-22 20:13:01 -0400

    git init

---
+++ app.js
@@ -0,0 +1,55 @@
+var currentList = {};
+
+//Main Function
+function createShoppingList() {
+   currentList.name = $("#shoppingListName").val();
+   currentList.items = new Array
+ 
+   $("#shoppingListTitle").html(currentList.name);
+   $("#shoppingListItems").empty();
+
+   $("#createListDiv").hide();
+   $("#shoppingListDiv").show();
+
+}
+
+function addItem() {
+   var newItem = {};
+   newItem.name = $("#newItemName").val();
+   currentList.items.push(newItem);
+   console.log(currentList);
+
+   drawItems();
+
+}
+
+function drawItems() {
+    var $list = $("#shoppingListItems").empty();
+
+    for(var i = 0; i < currentList.items.length; i++){
+        var currentItem = currentList.items[i];
+        var $li = $("<li>").html(currentItem.name).attr("id","item_" + i);
+        var $deleteBtn = $("<button onclick='deleteItem("+ i +")'>D</button>").appendTo($li);
+        var $checkBtn = $("<button onclick='checkItem("+ i + ")'>C</button>").appendTo($li);
+        $li.appendTo($list);
+    }
+
+}
+
+function deleteItem(index) {
+    currentList.items.splice(index, 1);
+    drawItems();
+} 
+
+function checkItem(index) {
+    if($("#item_" + index).hasClass("checked")){
+        $("#item_" + index).removeClass("checked");
+    } else {
+        $("#item_" + index).addClass("checked");
+    }
+}
+
+
+$(document).ready(function() {
+    console.log("ready");
+})
